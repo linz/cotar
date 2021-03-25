40514c25763d754a8b93475e208beacdf83a3143
Blayne Chard <blayne@chard.com>
2021-03-25 13:52:58 +1300

    feat(cli): configure the decompression before storing

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ const logger = process.stdout.isTTY ? pino(PrettyTransform.stream()) : pino();
 export class CreateCovt extends Command {
   static flags = {
     force: flags.boolean({ description: 'force overwriting existing files' }),
+    decompress: flags.boolean({
+      description: 'decompress gzip encoded tiles before storing in tar',
+      default: false,
+    }),
   };
 
   static args = [
@@ -29,7 +33,7 @@ export class CreateCovt extends Command {
       logger.error('Input file must be a mbtiles file');
       return;
     }
-    await toTTiles(args.inputFile, args.outputFile, logger);
+    await toTTiles(args.inputFile, args.outputFile, flags.decompress, logger);
     await toTTilesIndex(args.outputFile, args.outputFile + '.tari', logger);
   }
 }
