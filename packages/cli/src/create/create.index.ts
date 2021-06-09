facd3559b179f2723b35e359f4bd6800ac5b5b16
Blayne Chard <blayne@chard.com>
2021-06-09 14:33:20 +1200

    refactor: remove unused code

--- packages/cli/src/create/create.index.ts
+++ packages/cli/src/create/create.index.ts
@@ -4,20 +4,10 @@ import type { Logger } from 'pino';
 
 export async function toTarIndex(filename: string, indexFileName: string, logger: Logger): Promise<void> {
   const fd = await fs.open(filename, 'r');
-
-  const headBuffer = Buffer.alloc(512);
   logger.info({ index: indexFileName }, 'Cotar.Index:Start');
   const outputBuffer = createWriteStream(indexFileName);
-  outputBuffer.write(`[\n`);
-
   const startTime = Date.now();
 
-  async function readBytes(offset: number, count: number): Promise<Buffer | null> {
-    const res = await fd.read(headBuffer, 0, count, offset);
-    if (res.bytesRead < count) return null;
-    return headBuffer;
-  }
-
-  const fileCount = await TarReader.index(readBytes, outputBuffer, logger);
+  const fileCount = await TarReader.index(fd, outputBuffer, logger);
   logger.info({ index: indexFileName, count: fileCount, duration: Date.now() - startTime }, 'Cotar.Index:Created');
 }
