f9cad7687e569a727772d7d44ff1257f7f786452
Blayne Chard <bchard@linz.govt.nz>
2021-11-03 16:53:39 +1300

    refactor: lint all js and json files

--- packages/cli/bin/cotar.js
+++ packages/cli/bin/cotar.js
@@ -4,7 +4,7 @@ import { run, flush, Errors } from '@oclif/core';
 
 run(void 0, import.meta.url)
   .then(flush)
-  .catch(error => {
+  .catch((error) => {
     if (error.oclif) return Errors.handle(error);
     console.log(error);
   });
