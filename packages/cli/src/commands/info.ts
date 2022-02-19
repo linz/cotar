510b09289117eb063d0beca7bf08a20aec901504
Blayne Chard <bchard@linz.govt.nz>
2022-02-20 11:49:25 +1300

    feat(cli): validate seperate index files

--- packages/cli/src/commands/info.ts
+++ packages/cli/src/commands/info.ts
@@ -1,5 +1,5 @@
 import { SourceFile } from '@chunkd/source-file';
-import { Cotar, TarReader } from '@cotar/core';
+import { Cotar, CotarIndexBinary, TarReader } from '@cotar/core';
 import { Command, flags } from '@oclif/command';
 import { promises as fs } from 'fs';
 import { logger } from '../log.js';
@@ -10,7 +10,21 @@ export class CotarInfo extends Command {
     verbose: flags.boolean({ description: 'verbose logging' }),
   };
 
-  static args = [{ name: 'inputFile', required: true }];
+  static args = [
+    { name: 'inputFile', required: true },
+    { name: 'indexFile', required: false },
+  ];
+
+  async loadIndex(tarFile: string, indexFile?: string): Promise<CotarIndexBinary> {
+    if (indexFile == null) {
+      const source = new SourceFile(tarFile);
+      const ct = await Cotar.fromTar(source);
+      return ct.index;
+    }
+
+    const source = new SourceFile(indexFile);
+    return await CotarIndexBinary.create(source);
+  }
 
   async run(): Promise<void> {
     const { args, flags } = this.parse(CotarInfo);
@@ -18,13 +32,11 @@ export class CotarInfo extends Command {
 
     logger.info({ fileName: args.inputFile }, 'Cotar:Load');
 
-    const source = new SourceFile(args.inputFile);
-    const cotar = await Cotar.fromTar(source);
-
-    logger.info({ fileName: args.inputFile }, 'Cotar:Loaded');
+    const index = await this.loadIndex(args.inputFile, args.indexFile);
+    logger.info({ fileName: args.inputFile, indexFile: args.indexFile }, 'Cotar:Loaded');
 
     const fd = await fs.open(args.inputFile, 'r');
-    const fileCount = await TarReader.validate(fd, cotar.index, logger);
+    const fileCount = await TarReader.validate(fd, index, logger);
     await fd.close();
 
     logger.info({ fileName: args.inputFile, files: fileCount }, 'Cotar:Validated');
